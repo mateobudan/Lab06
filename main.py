1e8ee3e92b222d86e16adc24cb738f97e23c5106
Agustin Zurlo <aguszurlo@Aguss-MacBook-Pro.local>
2023-07-19 18:02:09 -0500

    Added the decode() function to the program

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 # Mateo Budan encode() method
+# Agustin Zurlo Redi decode() method
 def encode(password):
     encoded_pass = ''
     for num in password:
         new_num = str((int(num)+3) % 10)
         encoded_pass += new_num
     return encoded_pass
+
+
+def decode(encoded_pass):
+    decoded_pass = ''
+    for num in encoded_pass:
+        new_num = str((int(num) + 7) % 10)
+        decoded_pass += new_num
+    return decoded_pass
+
 
 if __name__ == '__main__':
     stored_password = ''
@@ -20,6 +30,13 @@ if __name__ == '__main__':
             password = input('Please enter your password to encode: ')
             stored_password = encode(password)
             print('Your password has been encoded and stored!\n')
+        if choice == 2:
+            if stored_password == "":
+                print("No password available to decode.\n")
+                continue
+            else:
+                decoded_password = decode(stored_password)
+                print("The encoded password is", stored_password, "and the original password is", decoded_password, end=".\n\n")
         if choice == 3:
             break
 
